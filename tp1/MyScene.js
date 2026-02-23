81f2ab9f652ac0fc47058cd126b963cfbb980f90
Cláudio Meireles <up202306618@edu.fe.up.pt>
2026-02-23 09:49:50 +0000

    Reformat

--- tp1/MyScene.js
+++ tp1/MyScene.js
@@ -6,93 +6,87 @@ import { MyDiamond } from "./MyDiamond.js";
  * @constructor
  */
 export class MyScene extends CGFscene {
-  constructor() {
-    super();
-  }
-  init(application) {
-    super.init(application);
-    
-    this.initCameras();
-    this.initLights();
+    constructor() {
+        super();
+    }
+    init(application) {
+        super.init(application);
 
-    //Background color
-    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        this.initCameras();
+        this.initLights();
 
-    this.gl.clearDepth(100.0);
-    this.gl.enable(this.gl.DEPTH_TEST);
-    this.gl.enable(this.gl.CULL_FACE);
-    this.gl.depthFunc(this.gl.LEQUAL);
+        //Background color
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-    //Initialize scene objects
-    this.axis = new CGFaxis(this);
-    this.diamond = new MyDiamond(this);
+        this.gl.clearDepth(100.0);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.enable(this.gl.CULL_FACE);
+        this.gl.depthFunc(this.gl.LEQUAL);
 
-    //Objects connected to MyInterface
-    this.displayAxis = true;
-    this.scaleFactor = 1;
-  }
-  initLights() {
-    this.lights[0].setPosition(15, 2, 5, 1);
-    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-    this.lights[0].enable();
-    this.lights[0].update();
-  }
-  initCameras() {
-    this.camera = new CGFcamera(
-      0.4,
-      0.1,
-      500,
-      vec3.fromValues(15, 15, 15),
-      vec3.fromValues(0, 0, 0)
-    );
-  }
-  setDefaultAppearance() {
-    this.setAmbient(0.2, 0.4, 0.8, 1.0);
-    this.setDiffuse(0.2, 0.4, 0.8, 1.0);
-    this.setSpecular(0.2, 0.4, 0.8, 1.0);
-    this.setShininess(10.0);
-  }
-  display() {
-    // ---- BEGIN Background, camera and axis setup
-    // Clear image and depth buffer everytime we update the scene
-    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
-    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-    // Initialize Model-View matrix as identity (no transformation
-    this.updateProjectionMatrix();
-    this.loadIdentity();
-    // Apply transformations corresponding to the camera position relative to the origin
-    this.applyViewMatrix();
+        //Initialize scene objects
+        this.axis = new CGFaxis(this);
+        this.diamond = new MyDiamond(this);
 
-    // Draw axis
-    if (this.displayAxis) this.axis.display();
+        //Objects connected to MyInterface
+        this.displayAxis = true;
+        this.scaleFactor = 1;
+    }
+    initLights() {
+        this.lights[0].setPosition(15, 2, 5, 1);
+        this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+        this.lights[0].enable();
+        this.lights[0].update();
+    }
+    initCameras() {
+        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
+    }
+    setDefaultAppearance() {
+        this.setAmbient(0.2, 0.4, 0.8, 1.0);
+        this.setDiffuse(0.2, 0.4, 0.8, 1.0);
+        this.setSpecular(0.2, 0.4, 0.8, 1.0);
+        this.setShininess(10.0);
+    }
+    display() {
+        // ---- BEGIN Background, camera and axis setup
+        // Clear image and depth buffer everytime we update the scene
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+        // Initialize Model-View matrix as identity (no transformation
+        this.updateProjectionMatrix();
+        this.loadIdentity();
+        // Apply transformations corresponding to the camera position relative to the origin
+        this.applyViewMatrix();
 
-    this.setDefaultAppearance();
+        // Draw axis
+        if (this.displayAxis) this.axis.display();
 
-    var sca = [
-      this.scaleFactor,
-      0.0,
-      0.0,
-      0.0,
-      0.0,
-      this.scaleFactor,
-      0.0,
-      0.0,
-      0.0,
-      0.0,
-      this.scaleFactor,
-      0.0,
-      0.0,
-      0.0,
-      0.0,
-      1.0,
-    ];
+        this.setDefaultAppearance();
 
-    this.multMatrix(sca);
+        var sca = [
+            this.scaleFactor,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            this.scaleFactor,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            this.scaleFactor,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+        ];
 
-    // ---- BEGIN Primitive drawing section
+        this.multMatrix(sca);
 
-    this.diamond.display();
+        // ---- BEGIN Primitive drawing section
 
-    // ---- END Primitive drawing section
-  }
+        this.diamond.display();
+
+        // ---- END Primitive drawing section
+    }
 }
