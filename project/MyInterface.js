64bda29f984a960ac18b3d5644ba3d7965187d1d
Miguel Pereira <up202304387@edu.fe.up.pt>
2026-04-20 10:28:29 +0100

    project innit

---
+++ project/MyInterface.js
@@ -0,0 +1,21 @@
+import { CGFinterface, dat } from "../lib/CGF.js";
+
+export class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        super.init(application);
+
+        // init GUI. For more information on the methods, check:
+        // https://github.com/dataarts/dat.gui/blob/master/API.md
+        this.gui = new dat.GUI();
+
+        this.gui.add(this.scene, "scaleFactor", 0.1, 5).name("Scale Factor");
+        this.gui.add(this.scene, "displayAxis").name("Display Axis");
+        this.gui.add(this.scene, 'selectedObject', this.scene.objectIDs).name('Selected Object')
+
+        return true;
+    }
+}
