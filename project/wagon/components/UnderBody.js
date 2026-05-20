6caab1c4d902928bf8cb47bdb433b7790ef7acd5
Cláudio Meireles <up202306618@edu.fe.up.pt>
2026-05-20 18:57:00 +0100

    refactor(wagon): tidy UnderBody with snake_case vars and display helpers

--- project/wagon/components/UnderBody.js
+++ project/wagon/components/UnderBody.js
@@ -3,40 +3,75 @@ import { Direction } from "./Direction.js";
 import { Wheel } from "./Wheel.js";
 import { Beam } from "./Beam.js";
 
-/**
- * UnderBody
- * @constructor
- * @param scene - Reference to MyScene object
- */
 export class UnderBody extends CGFGroup {
-    constructor(scene, steeringAngle) {
+    // =====================================================
+    // Init
+    // =====================================================
+
+    constructor(scene, steering_angle) {
         super(scene);
-        this.direction = this.addPart(new Direction(this.scene, 5.5, steeringAngle));
+
+        this.direction = this.addPart(new Direction(this.scene, 5.5, steering_angle));
         this.wheel = this.addPart(new Wheel(this.scene));
         this.beam = this.addPart(new Beam(this.scene, 5.5, 0.2));
-        this.bigBeam = this.addPart(new Beam(this.scene, 9, 0.2));
+        this.big_beam = this.addPart(new Beam(this.scene, 9, 0.2));
     }
 
+    // =====================================================
+    // Update
+    // =====================================================
+
+    updateDirection(angle) {
+        this.direction.updateDirection(angle);
+    }
+
+    // =====================================================
+    // Display
+    // =====================================================
+
     display() {
-        // Direction (with 2 wheels)
         this.scene.pushMatrix();
+
+        // Move to the front axle frame; the rest of the chassis is laid out
+        // relative to it.
         this.scene.translate(0, 1, 5.5);
+
+        // Steering direction (with its 2 front wheels)
         this.direction.display();
+
+        // Lift to the support-beam layer
         this.scene.translate(0, 0.4, 0);
-        // Direction support Beam
+
+        this.displayFrontBeam();
+        this.displayLowerBeams();
+        this.displayTopBeams();
+        this.displayBackStructure();
+        this.displayBackWheels();
+
+        this.scene.popMatrix();
+    }
+
+    displayFrontBeam() {
+        // Cross beam supporting the steering direction
         this.scene.pushMatrix();
         this.scene.rotate(Math.PI / 2, 0, 1, 0);
-        this.scene.scale(1.5,1,0.65);
+        this.scene.scale(1.5, 1, 0.65);
         this.beam.display();
         this.scene.popMatrix();
-        // Layer 2 support Beams
+    }
+
+    displayLowerBeams() {
+        // Pair of long beams running the length of the chassis
         this.scene.pushMatrix();
         this.scene.translate(1.5, 0.4, -4.5);
-        this.bigBeam.display();
+        this.big_beam.display();
         this.scene.translate(-3, 0, 0);
-        this.bigBeam.display();
+        this.big_beam.display();
         this.scene.popMatrix();
-        // Top layer support beams
+    }
+
+    displayTopBeams() {
+        // Three evenly spaced cross beams on the top layer
         this.scene.pushMatrix();
         this.scene.translate(0, 0.8, -0.4);
         this.scene.scale(0.7, 1, 1);
@@ -47,33 +82,32 @@ export class UnderBody extends CGFGroup {
         this.scene.translate(4, 0, 0);
         this.beam.display();
         this.scene.popMatrix();
-        // Back structure
+    }
+
+    displayBackStructure() {
+        // Cross beams bracing the rear axle
         this.scene.pushMatrix();
-        this.scene.translate(0,0, -8);
+        this.scene.translate(0, 0, -8);
         this.scene.rotate(Math.PI / 2, 0, 1, 0);
-        // back wheel support beam
+        // Back wheel support beam
         this.scene.pushMatrix();
-        this.scene.translate(0,0,0);
-        this.scene.scale(1.5,1,0.65);
+        this.scene.scale(1.5, 1, 0.65);
         this.beam.display();
         this.scene.popMatrix();
-        this.scene.translate(0,-0.4,0);
+        this.scene.translate(0, -0.4, 0);
         this.beam.display();
-        this.scene.popMatrix();
-        // Back wheels
-        this.scene.translate(5.5 / 2,-0.4,-8);
-        this.scene.rotate(Math.PI / 2, 0, 1, 0);
-        this.wheel.display();
-        this.scene.translate(0,0,-5.5);
-        this.scene.rotate(Math.PI, 0,1,0);
-        this.wheel.display();
-
-
-
         this.scene.popMatrix();
     }
 
-    updateDirection(angle){
-        this.direction.updateDirection(angle);
+    displayBackWheels() {
+        // Rear axle wheels (mirrored left and right)
+        this.scene.pushMatrix();
+        this.scene.translate(5.5 / 2, -0.4, -8);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.wheel.display();
+        this.scene.translate(0, 0, -5.5);
+        this.scene.rotate(Math.PI, 0, 1, 0);
+        this.wheel.display();
+        this.scene.popMatrix();
     }
 }
