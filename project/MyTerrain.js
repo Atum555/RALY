da809d10da8850b5561615c2578a8bb0bbd09e6b
Cláudio Meireles <up202306618@edu.fe.up.pt>
2026-05-20 22:36:27 +0100

    refactor(terrain): inline plane geometry and drop MyPlane dependency

--- project/MyTerrain.js
+++ project/MyTerrain.js
@@ -1,11 +1,12 @@
 import { CGFobject, CGFshader, CGFtexture } from "../lib/CGF.js";
-import { MyPlane } from "./sky/MyPlane.js";
 
 export class MyTerrain extends CGFobject {
-    constructor(scene) {
+    constructor(scene, nrDivs = 20) {
         super(scene);
 
-        this.plane = new MyPlane(scene, 20);
+        this.nrDivs = nrDivs;
+        this.initBuffers();
+
         // Multi-texturing shader: Blends Grass (Unit 0) and Dirt (Unit 1) based on the Red channel of the Splat Map (Unit 2).
         // Note: Vertex displacement (heightmap) logic will be injected here in a future sprint.
         this.shader = new CGFshader(
@@ -25,6 +26,44 @@ export class MyTerrain extends CGFobject {
         });
     }
 
+    initBuffers() {
+        const patchLength = 1.0 / this.nrDivs;
+
+        // Generate vertices, normals, and texCoords
+        this.vertices = [];
+        this.normals = [];
+        this.texCoords = [];
+        let yCoord = 0.5;
+        for (let j = 0; j <= this.nrDivs; j++) {
+            let xCoord = -0.5;
+            for (let i = 0; i <= this.nrDivs; i++) {
+                this.vertices.push(xCoord, yCoord, 0);
+                this.normals.push(0, 0, 1);
+                this.texCoords.push(i / this.nrDivs, j / this.nrDivs);
+                xCoord += patchLength;
+            }
+            yCoord -= patchLength;
+        }
+
+        // Generating indices
+        this.indices = [];
+        let ind = 0;
+        for (let j = 0; j < this.nrDivs; j++) {
+            for (let i = 0; i <= this.nrDivs; i++) {
+                this.indices.push(ind);
+                this.indices.push(ind + this.nrDivs + 1);
+                ind++;
+            }
+            if (j + 1 < this.nrDivs) {
+                this.indices.push(ind + this.nrDivs);
+                this.indices.push(ind);
+            }
+        }
+
+        this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
+        this.initGLBuffers();
+    }
+
     display() {
         this.scene.setActiveShader(this.shader);
         this.grassTexture.bind(0);
@@ -37,7 +76,7 @@ export class MyTerrain extends CGFobject {
             uSamplerMap: 2,
         });
 
-        this.plane.display();
+        super.display();
         this.scene.setActiveShader(this.scene.defaultShader);
     }
 }
