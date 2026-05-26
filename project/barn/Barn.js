181be1caac3a3dcc2f2e27f76959d78bd3d8355b
Miguel Pereira <up202304387@edu.fe.up.pt>
2026-05-26 18:46:47 +0100

    corrected circle radius and position

--- project/barn/Barn.js
+++ project/barn/Barn.js
@@ -2,11 +2,15 @@ import { CGFGroup } from "../core/CGFGroup.js";
 import { Circle } from "./Circle.js";
 import { Cube } from "./Cube.js";
 import { Prism } from "./Prism.js";
+//import { Roof } from "./Roof.js";
 
 export class Barn extends CGFGroup {
     // =====================================================
     // Init
     // =====================================================
+// TODO Add shader for windows and door, essentially allowing it to have a second and third texture
+// TODO since we're already gonna be using a shader why not also do it for the white beams that can be added detail to the barn
+// TODO Fix the roof
 
     constructor(scene, radius = 96, elevation = 0) {
         super(scene);
@@ -17,14 +21,15 @@ export class Barn extends CGFGroup {
         this.length = 48;
         this.width = 48;
         this.header = 10;
+        this.pickup = {x: 0, z: 48, r : this.length / 2}
         this.initComponents();
     }
 
     initComponents() {
         this.cube = this.addPart(new Cube(this.scene, this.top, this.opening, this.length, this.header, this.width));
         this.prism = this.addPart(new Prism(this.scene, this.width, this.top, this.length));
-        this.roof = null;
-        this.circle = this.addPart(new Circle(this.scene, this.radius, 50));
+        //this.roof = this.addPart(new Roof(this.scene,this.width,this.top,this.length,2,5,5));
+        this.circle = this.addPart(new Circle(this.scene, this.pickup["r"], 50));
     }
 
 
@@ -40,9 +45,12 @@ export class Barn extends CGFGroup {
         this.cube.display();
         this.scene.translate(0, this.top, 0);
         this.prism.display();
-
+//        this.roof.display();
         this.scene.popMatrix();
+        this.scene.pushMatrix();
+        this.scene.translate(this.pickup["x"], 0, this.pickup["z"]);
         this.circle.display();
+        this.scene.popMatrix();
         this.scene.popMatrix();
     }
 }
