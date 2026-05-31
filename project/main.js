0333d7afda28b59a9b3fc5ff2f3dd12ef16fc568
Miguel Pereira <up202304387@edu.fe.up.pt>
2026-06-01 00:48:28 +0100

    initial readme version

--- project/main.js
+++ project/main.js
@@ -2,6 +2,22 @@ import { CGFapplication } from "../lib/CGF.js";
 import { Scene } from "./Scene.js";
 import { UI } from "./UI.js";
 
+(function () {
+    const originalGetContext = HTMLCanvasElement.prototype.getContext;
+    HTMLCanvasElement.prototype.getContext = function (type, attributes) {
+        if (
+            type === "webgl" ||
+            type === "webgl2" ||
+            type === "experimental-webgl"
+        ) {
+            attributes = attributes || {};
+            attributes.powerPreference = "high-performance";
+            attributes.failIfMajorPerformanceCaveat = false;
+        }
+        return originalGetContext.call(this, type, attributes);
+    };
+})();
+
 function main() {
     const app = new CGFapplication(document.body);
     const scene = new Scene();
